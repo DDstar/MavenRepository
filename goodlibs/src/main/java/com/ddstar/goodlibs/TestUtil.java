08668b94636e3e45603f7896a7523f67b21b3064
DDstar <DDstar@gmail.com>
2018-07-27 13:35:47 +0800

    首次添加测试

---
+++ goodlibs/src/main/java/com/ddstar/goodlibs/TestUtil.java
@@ -0,0 +1,12 @@
+package com.ddstar.goodlibs;
+
+import android.util.Log;
+
+/**
+ * Created by DDStar on 2018/7/27.
+ */
+public class TestUtil {
+    public static void sayHello() {
+        Log.e("sayHello", "hello");
+    }
+}
